7ba7981f40477fceeb0a172bb8a68a0cd2511667
Matt Welch <mwelch2489@gmail.com>
2018-05-01 20:26:57 -0400

    added end estimate classes. Still does not display

--- LandscapeAppProject/app/src/main/java/com/example/matt/myfirstapp/EndActivity.java
+++ LandscapeAppProject/app/src/main/java/com/example/matt/myfirstapp/EndActivity.java
@@ -1,31 +1,60 @@
 package com.example.matt.myfirstapp;
 
-import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
-import android.widget.TextView;
+import android.widget.ListView;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class EndActivity extends AppCompatActivity {
-
-    TextView endList;
+    String[] trees;
+    String[] hardgoods;
+    String[] flowers;
+    String[] grass;
+    String[] shrubs;
+    String[] labor;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_end);
+        setContentView(R.layout.activity_tree);
 
-        TextView endList = (TextView) findViewById(R.id.EndList);
+        Resources res = getResources();
+        trees = res.getStringArray(R.array.trees);
+        hardgoods = res.getStringArray(R.array.hardgoods);
+        flowers = res.getStringArray(R.array.flowers);
+        grass = res.getStringArray(R.array.grass);
+        shrubs = res.getStringArray(R.array.shrubs);
+        labor = res.getStringArray(R.array.labor);
 
+        List <String> temp = new ArrayList<>();
+        Collections.addAll(temp, trees);
+        Collections.addAll(temp, hardgoods);
+        Collections.addAll(temp, flowers);
+        Collections.addAll(temp, grass);
+        Collections.addAll(temp, shrubs);
+        Collections.addAll(temp, labor);
+
+        String[] items = temp.toArray(new String[temp.size()]);
+
+        ListView EndList = (ListView) findViewById(R.id.EndList);
+
+        ListAdapter EndActivityAdapter = new EndActivityAdapter(this, items);
+        EndList.setAdapter(EndActivityAdapter);
+
+        EndList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 
-    public void displayEstimateItems(View view){
-        SharedPreferences settings = getSharedPreferences("SavedItems", 0);
-
-        String estimateItems =  settings.getString("SavedItems", "No Items Were Selected");
-        endList.setText(estimateItems);
-    }
 }
---
+++ LandscapeAppProject/app/src/main/java/com/example/matt/myfirstapp/EndActivityAdapter.java
@@ -0,0 +1,59 @@
+package com.example.matt.myfirstapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class EndActivityAdapter extends BaseAdapter {
+    String[] items;
+    LayoutInflater inflate;
+    Context Ctx;
+    SharedPreferences Settings;
+
+    public EndActivityAdapter(Context ctx, String[] Items) {
+        items = Items;
+        inflate = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Ctx = ctx;
+    }
+
+    @Override
+    public int getCount() { return items.length;}
+
+    @Override
+    public Object getItem(int i) {
+        return items[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View v = inflate.inflate(R.layout.activity_end_layout, null);
+        TextView EndTxtVw = (TextView) v.findViewById(R.id.EstimateItemTxt);
+        final TextView countTxtVw = (TextView) v.findViewById(R.id.EstimateCountTxt);
+
+        String Item = items[i];
+        EndTxtVw.setText(Item);
+        int setValue = 0;
+        Settings = Ctx.getSharedPreferences("SavedItems", 0);
+        if(Settings.contains(Item)) {
+            setValue = Settings.getInt(Item, 0);
+        }
+        SharedPreferences.Editor edit = Settings.edit();
+        countTxtVw.setText(String.valueOf(setValue));
+
+        edit.putInt(Item, setValue);
+        edit.commit();
+
+        return v;
+    }
+}
